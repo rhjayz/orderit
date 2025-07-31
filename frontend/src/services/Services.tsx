ab088d80aaa6d31ad8ecc253b91242ab03fa4002
rhjayz <fikribima123@gmail.com>
2025-07-31 17:47:08 +0700

    Configuration Vercel BackendURL

--- frontend/src/services/Services.tsx
+++ frontend/src/services/Services.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.BACKEND_URL;
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 ///AUTH CONFIGURATION
 export const register = async (userData: {
