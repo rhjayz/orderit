e42a63cfebcfe2c28581c5130a87a9ff5327fb8e
rhjayz <fikribima123@gmail.com>
2025-08-01 06:54:24 +0700

    fix cookies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,9 +33,7 @@ console.log("GOOGLE_CALLBACK_URL:", process.env.GOOGLE_CALLBACK_URL);
 
 app.use("/api", route);
 
-if (process.env.NODE_ENV === "production") {
-  console.log("Running in production mode");
-}
+console.log("NODE_ENV", process.env.NODE_ENV);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
