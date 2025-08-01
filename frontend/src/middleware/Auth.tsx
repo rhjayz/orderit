d94217fe83ba2f73d99c124a72614a63464c8b7e
rhjayz <fikribima123@gmail.com>
2025-08-01 08:18:41 +0700

    Fix udh dong

--- frontend/src/middleware/Auth.tsx
+++ frontend/src/middleware/Auth.tsx
@@ -1,22 +1,29 @@
-  import { Outlet, useLocation, useNavigate } from "react-router-dom";
-  import { useAuth } from "../context/AuthContext";
-  import { useEffect } from "react";
-  
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { useEffect } from "react";
 
-  type AuthMiddlewareCheck = {
+type AuthMiddlewareCheck = {
   status: boolean; // Status kontrol middleware
-  };
+};
 
-  const Auth: React.FC<AuthMiddlewareCheck> = ( status ) =>  {
-    const { token, user } = useAuth();
-    const location = useLocation();
-    const navigate = useNavigate();
-    
-    useEffect(() => {
-    if (!token) {
-        
+const Auth: React.FC<AuthMiddlewareCheck> = (status) => {
+  const { user } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
       console.log("User not logged in, redirecting to /login");
-      if (!["/login", "/register", "/forgotpassword", "/newpassword", "/resultresetpassword", "/"].includes(location.pathname)) {
+      if (
+        ![
+          "/login",
+          "/register",
+          "/forgotpassword",
+          "/newpassword",
+          "/resultresetpassword",
+          "/",
+        ].includes(location.pathname)
+      ) {
         navigate("/login", { replace: true });
       }
       return;
@@ -28,26 +35,33 @@
         navigate("/verifyaccount", { replace: true });
       }
       return;
-      }
-      
-      if (["/login", "/register", "/forgotpassword", "/newpassword", "/resultresetpassword", "/"].includes(location.pathname)) {
-    console.log("User already logged in, redirecting...");
-
-    // Jika sebelumnya ada halaman lain, balik ke halaman sebelum login
-    if (window.history.length > 2) {
-      navigate(-1);
-    } else {
-      // Kalau history kosong, arahkan ke /dashboard tanpa merusak branch lain
-      navigate("/dashboard", { replace: true });
     }
-  }
+
+    if (
+      [
+        "/login",
+        "/register",
+        "/forgotpassword",
+        "/newpassword",
+        "/resultresetpassword",
+        "/",
+      ].includes(location.pathname)
+    ) {
+      console.log("User already logged in, redirecting...");
+
+      // Jika sebelumnya ada halaman lain, balik ke halaman sebelum login
+      if (window.history.length > 2) {
+        navigate(-1);
+      } else {
+        // Kalau history kosong, arahkan ke /dashboard tanpa merusak branch lain
+        navigate("/dashboard", { replace: true });
+      }
+    }
 
     console.log("User verified, allowing access");
-    
-  }, [token, user?.verified, navigate, location.pathname, status]);
+  }, [user, user?.verified, navigate, location.pathname, status]);
 
-    
-    return <Outlet />;    
-  };
+  return <Outlet />;
+};
 
-  export default Auth;
+export default Auth;
